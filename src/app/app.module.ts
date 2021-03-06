40ca859f6c7450be6e94e3d503016a0ebcd79363
Mariana Estrada Calle <mestrada@sistecredito.com>
2021-03-06 12:02:50 -0500

    project initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { DirectivaPruebaDirective } from './directiva-prueba.directive';
+import { PipePruebaPipe } from './pipe-prueba.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundoComponent,
+    DirectivaPruebaDirective,
+    PipePruebaPipe
   ],
   imports: [
     BrowserModule
---
+++ src/app/directiva-prueba.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DirectivaPruebaDirective } from './directiva-prueba.directive';
+
+describe('DirectivaPruebaDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DirectivaPruebaDirective();
+    expect(directive).toBeTruthy();
+  });
+});
